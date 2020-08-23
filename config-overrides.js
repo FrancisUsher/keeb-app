05c27f8660d1977b05eb68ef9f7e0160e6160e09
Francis Usher <francis.w.usher@gmail.com>
2020-08-22 17:57:52 -0700

    Convert to a vue project.

--- config-overrides.js
+++
@@ -1,9 +0,0 @@
-/* config-overrides.js */
-const { useBabelRc, addWebpackAlias, override } = require('customize-cra');
-
-module.exports = override(
-  useBabelRc(),
-  addWebpackAlias({
-    tinyqueue$: require.resolve('tinyqueue/tinyqueue.js'),
-  })
-);
--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-it('asks people to help improve', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/improve keeb.app/i);
-  expect(linkElement).toBeInTheDocument();
-});
